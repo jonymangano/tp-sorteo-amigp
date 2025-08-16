d73f42ab8caff7d2a502c3e7635d8308cb0b2012
jonymangano <165599947+jonymangano@users.noreply.github.com>
2025-08-15 22:30:46 -0300

    Creando funciones

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+alert('¡Bienvenido al juego del Amigo Secreto!');
+
+function agregarAmigo() {
+    let nombre = document.getElementById('nombre').value;
+    
+    if (nombre === '') {
+        alert('Por favor, ingresa un nombre válido.');
+        return;
+    } else if (amigos.includes(nombre)) {
+        alert('Este nombre ya fue ingresado. Por favor, ingresa un nombre diferente.');
+        return;
+    } else {
+        amigos.push(nombre);
+    limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#nombre').value = '';   
+}
