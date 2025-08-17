982573f49299be5f1d5e11aed638d93efe365dca
jonymangano <165599947+jonymangano@users.noreply.github.com>
2025-08-16 23:33:12 -0300

    Agregando mas funciones

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-alert('¡Bienvenido al juego del Amigo Secreto!');
+
 
 function agregarAmigo() {
-    let nombre = document.getElementById('nombre').value;
+    let nombre = document.getElementById('amigo').value;
     
     if (nombre === '') {
         alert('Por favor, ingresa un nombre válido.');
+        limpiarCaja();
         return;
     } else if (amigos.includes(nombre)) {
-        alert('Este nombre ya fue ingresado. Por favor, ingresa un nombre diferente.');
+        alert('Este nombre ya fue ingresado. Si tenes 2 o más amigos con el mismo nombre por favor diferencialos.');
+        limpiarCaja();
         return;
     } else {
         amigos.push(nombre);
+        imprimirUnaLista('listaAmigos', amigos);
+        capitalizar('listaAmigos');
     limpiarCaja();
     }
+    console.log(amigos);
     return;
 }
 
+function imprimirUnaLista(id, lista) {
+    let elementoHTML = document.getElementById(id);
+    elementoHTML.innerHTML = '';
+    for (lista of lista) {
+        elementoHTML.innerHTML += `<li>${lista}</li>`;
+    }
+        return;
+}
+
+function capitalizar(id) {
+    const elemento = document.getElementById(id);
+    elemento.style.textTransform = 'capitalize';
+}
+
 function limpiarCaja() {
-    document.querySelector('#nombre').value = '';   
+    document.querySelector('#amigo').value = '';   
 }
